2dfd7da88f6c6677558a1200359cb727fcc422f8
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2023-01-05 11:07:46 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+// start server : php -S 127.0.0.1:8000
+require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . "/../src/app.php";
+
---
+++ src/app.php
@@ -0,0 +1,8 @@
+<?php
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
